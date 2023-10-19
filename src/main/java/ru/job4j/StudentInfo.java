9a252fdebb4803466167305fd97a5889f3a8759e
Elfrigan <elfrigan@gmail.com>
2023-10-19 23:14:19 +0300

    1.1.1.4.1. Разбор первой программы [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("German Gordienko");
+        System.out.println("19.07.1973");
+    }
+}
